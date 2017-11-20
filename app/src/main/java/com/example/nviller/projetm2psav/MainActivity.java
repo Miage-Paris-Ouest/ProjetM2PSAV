5ae42d3feddd000d7577c6ac45eebed849459868
Hamlix <shirohamlix@gmail.com>
2017-11-20 21:45:43 +0100

    Test commit

--- app/src/main/java/com/example/nviller/projetm2psav/MainActivity.java
+++ app/src/main/java/com/example/nviller/projetm2psav/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Un super commentaire de test
         setContentView(R.layout.activity_main);
     }
 }
