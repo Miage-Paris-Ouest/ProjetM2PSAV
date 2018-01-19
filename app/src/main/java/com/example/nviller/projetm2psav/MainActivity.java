5d02906a8cbe39ebc2ffdb2b80c4b03f00eca7c8
Hamlix <shirohamlix@gmail.com>
2018-01-19 14:31:04 +0100

    MAJ Android

--- app/src/main/java/com/example/nviller/projetm2psav/MainActivity.java
+++ app/src/main/java/com/example/nviller/projetm2psav/MainActivity.java
@@ -1,22 +1,51 @@
 package com.example.nviller.projetm2psav;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.example.nviller.projetm2psav.adapter.SlidingMenuAdapter;
+import com.example.nviller.projetm2psav.fragment.Fragment1;
+import com.example.nviller.projetm2psav.fragment.Fragment2;
+import com.example.nviller.projetm2psav.fragment.Fragment3;
+import com.example.nviller.projetm2psav.model.ItemSlideMenu;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private EditText emailUser;
     private EditText passwordUser;
+    //private List<ItemSlideMenu> listSliding;
+    private List<ItemSlideMenu> listSliding;
+    private SlidingMenuAdapter adapter;
+    private ListView listViewSliding;
+    private DrawerLayout drawerLayout;
+    //private RelativeLayout mainContent;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Un super commentaire de test
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
         /*emailUser = (EditText) findViewById(R.id.email_user_ma);
@@ -54,5 +83,111 @@ public class MainActivity extends AppCompatActivity {
 
     private void updateUI(FirebaseUser currentUser) {
 
+        //Init component
+        listViewSliding = (ListView)findViewById(R.id.lv_sliding_menu);
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        //mainContent = (RelativeLayout)findViewById(R.id.main_content);
+        listSliding = new ArrayList<>();
+
+        //Add item for sliding list
+        listSliding.add(new ItemSlideMenu(R.drawable.setting,"Setting"));
+        listSliding.add((new ItemSlideMenu(R.drawable.about, "About")));
+        listSliding.add(new ItemSlideMenu(R.mipmap.ic_launcher,"Android"));
+        adapter = new SlidingMenuAdapter(this, listSliding);
+        listViewSliding.setAdapter(adapter);
+
+        //Display icon to open/close sliding list
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Set tittle
+        setTitle(listSliding.get(0).getTitle());
+
+        //Item selected
+        listViewSliding.setItemChecked(0, true);
+
+        //Close menu
+        drawerLayout.closeDrawer(listViewSliding);
+
+        //Display fragment 1 when start
+        replaceFragment(0);
+
+        //handle on item click
+        listViewSliding.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                //Set title
+                setTitle(listSliding.get(position).getTitle());
+                //Item selected
+                listViewSliding.setItemChecked(position, true);
+                //Replace fragment
+                replaceFragment(position);
+                //Close menu
+                drawerLayout.closeDrawer(listViewSliding);
+
+            }
+        });
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_opened, R.string.drawer_closed){
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                invalidateOptionsMenu();
+            }
+        };
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        //drawerLayout.setDrawerListener(actionBarDrawerToggle);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+
+    //Create method replace fragment
+    private void  replaceFragment(int pos){
+        Fragment fragment = null;
+        switch (pos){
+            case 0:
+                fragment = new Fragment1();
+                break;
+            case 1:
+                fragment = new Fragment2();
+                break;
+            case 2:
+                fragment = new Fragment3();
+                break;
+            default:
+                fragment = new Fragment1();
+                break;
+        }
+        if (null != fragment){
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            transaction.replace(R.id.main_content, fragment);
+            transaction.addToBackStack(null);
+            transaction.commit();
+        }
     }
 }
